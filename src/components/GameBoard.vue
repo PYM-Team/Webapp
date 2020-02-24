886ec6b15d1c591ac19eec049ae23e8c6b602256
Tom Sampic <tom.sampic@insa-lyon.fr>
2020-02-24 23:06:05 +0100

    #2 : creating a board of fake games template cards

---
+++ src/components/GameBoard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="board">
+    <div class="columns is-multiline is-desktop">
+      <div class="column is-3" v-for="template in templates" v-bind:key="template">
+          <TemplateCard :title="template.name" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import TemplateCard from '@/components/TemplateCard.vue';
+
+export default {
+  name: 'GameBoard',
+  components: { TemplateCard },
+  data() {
+    return {
+      templates: [{ name: 'murder at TC' }, { name: 'strange story hmm' }, { name: 'murder in the museum' }, { name: 'not a murder at TC' }, { name: 'not a murder at TC' }, { name: 'not a murder at TC' }],
+    };
+  },
+};
+</script>
---
+++ src/components/TemplateCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="card">
+    <header class="card-header">
+      <p class="card-header-title">
+        {{title}}
+      </p>
+      <a href="#" class="card-header-icon" aria-label="more options">
+        <span class="icon">
+          <i class="fas fa-angle-down" aria-hidden="true"></i>
+        </span>
+      </a>
+    </header>
+    <div class="card-content">
+      <div class="content">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque sed blanditiis,
+        assumenda repellendus tempore voluptatum eum! Aspernatur nam doloremque asperiores.
+        Expedita vero ducimus alias repudiandae dolorem distinctio officia tempora quas.
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a href="#" v-on:click="createGame" class="card-footer-item">Create a game</a>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TemplateCard',
+  props: {
+    title: String,
+  },
+  methods: {
+    createGame() {
+      fetch('https://rpplanner-api.herokuapp.com/api/games', {
+        method: 'post',
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          alert(`game created ${data.id}`);
+        })
+        .catch(() => {
+          console.log('error');
+        });
+    },
+  },
+};
+</script>

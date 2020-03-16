9d67e1ffa5dc9708857556608b40142f21d99d59
Tom Sampic <tom.sampic@insa-lyon.fr>
2020-03-16 14:41:15 +0100

    #3 basic dashboard page

---
+++ src/components/PlayerView.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="tile is-ancestor">
+  <tile class="is-vertical is-12">
+    <div class="tile is-6 is-parent">
+        <article class="tile is-child notification is-dark">
+          <a href="#">Players</a>
+        </article>
+      </div>
+      <div class="tile is-6 is-parent">
+        <article class="tile is-child notification is-light">
+          <a href="#">Missions</a>
+        </article>
+    </div>
+  </tile>
+</div>
+</template>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="container">
+  <div class="dahsboard">
+    <NavBar />
     <div class="title is-1">This will be a pretty Dashboard</div>
+    <PlayerView />
   </div>
 </template>
 
 <script>
+
+import NavBar from '@/components/DashboardNavBar.vue';
+import PlayerView from '@/components/PlayerView.vue';
+
 export default {
   name: 'Dashboard',
+  components: {
+    NavBar,
+    PlayerView,
+  },
 };
 </script>

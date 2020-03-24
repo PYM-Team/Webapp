7e6b6e275bd587883d5be8064a320f79b64bafd8
Tom Sampic <tom.sampic@insa-lyon.fr>
2020-03-24 18:05:19 +0100

    #6 displaying connected players list in dashboard

--- src/components/PlayerView.vue
+++ src/components/PlayerView.vue
@@ -1,28 +1,33 @@
 <template>
-<div class="tile is-ancestor">
-  <div class="tile is-6 is-parent">
-    <article class="tile is-child notification is-dark">
-      <h1>Players</h1>
-      <p>Vous avez {{nombrePlayers}} connectés</p>
-    </article>
+  <div class="tile is-ancestor">
+    <div class="tile is-6 is-parent">
+      <article class="tile is-child notification is-dark">
+        <h1>Players</h1>
+        <p>Liste des players</p>
+        <ul>
+          <li v-for="(player, key) in players" v-bind:key="key">
+            <span v-if="player.connected">{{ key }}</span>
+          </li>
+        </ul>
+      </article>
+    </div>
+    <div class="tile is-6 is-parent">
+      <article class="tile is-child notification is-link">
+        <h1>Running Game</h1>
+        <p>{{nameGame}}</p>
+        <p>Game Number : {{idGame}}</p>
+      </article>
+    </div>
   </div>
-  <div class="tile is-6 is-parent">
-    <article class="tile is-child notification is-link">
-      <h1>Running Game</h1>
-      <p>{{nameGame}}</p>
-      <p> Game Number : {{idGame}}</p>
-    </article>
-  </div>
-</div>
 </template>
 
 <script>
 export default {
   name: 'PlayerView',
   props: {
-    nombrePlayers: {
-      type: Number,
-      default: 900,
+    players: {
+      type: Object,
+      default: () => {},
     },
     nameGame: {
       type: String,
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,14 +1,14 @@
+
 <template>
   <div class="dahsboard">
     <NavBar />
     <div class="title is-1">This will be a pretty Dashboard</div>
-    <b-button @click="incrPlayers">Increment players number</b-button>
-    <PlayerView :nombrePlayers  ="this.nbPlayers" :nameGame ="this.nameGame" :idGame="this.idGame"/>
+    <PlayerView :players="this.players" :nameGame="this.nameGame" :idGame="this.idGame" />
   </div>
 </template>
 
 <script>
-
+/* eslint-disable no-unused-vars */
 import NavBar from '@/components/DashboardNavBar.vue';
 import PlayerView from '@/components/PlayerView.vue';
 import io from 'socket.io-client';
@@ -31,27 +31,21 @@ export default {
   },
   data() {
     return {
-      nbPlayers: 0,
+      players: {},
       socket: io('localhost:1337'),
     };
-  },
-  methods: {
-    incrPlayers() {
-      this.nbPlayers += 1;
-      console.log(this.nbPlayers);
-    },
   },
   mounted() {
     // on atend la fin du chargement de la page pour commencer les écoutes
     this.socket.emit('createGame', 101938);
 
-    this.socket.on('playerConnected', () => {
+    this.socket.on('playerConnected', (playerlist) => {
       console.log('New player');
-      this.nbPlayers += 1;
+      this.players = playerlist;
     });
-    this.socket.on('playerDisconnected', () => {
+    this.socket.on('playerDisconnected', (playerlist) => {
       console.log('Lost a player');
-      this.nbPlayers -= 1;
+      this.players = playerlist;
     });
   },
 };

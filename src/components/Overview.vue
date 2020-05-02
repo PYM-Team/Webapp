6aaf8c31992ad83c3f8130655910f726899f9178
LeonardGr <leonard.grynfogel@gmail.com>
2020-05-02 13:40:55 +0200

    #14 Creation - Creation of the overview page

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
-    <article class="notification is-primary title-font">
-      <h1>Running Game</h1>
-      <p>{{this.$store.state.gameTemplate}}</p>
-      <p>Game Number : {{this.$store.state.gameId}}</p>
-    </article>
-    <button class="button is-primary is-medium"
-            @click="isAnnModalActive = true">
-            Make an annoncement
-    </button>
-    <b-modal :active.sync="isAnnModalActive" scroll="keep">
+<div id = "Overview">
+      <article class="notification is-primary title-font ">
+        <h1>Running Game</h1>
+        <p>{{this.$store.state.gameTemplate}}</p>
+        <p>Game Number : {{this.$store.state.gameId}}</p>
+      </article>
+      <button class="button is-primary is-medium"
+        @click="isAnnModalActive = true">
+        Make an annoncement
+      </button>
+     <b-modal :active.sync="isAnnModalActive" scroll="keep">
         <div class="card">
               <div class="card-header">
                   <p class="card-header-title is-centered player-name">Annoucement</p>
@@ -21,9 +21,73 @@
                   <button class="button is-medium is-centered is-primary" @click="sendAnnounce()">Send it !</button>
               </div>
         </div>
-    </b-modal>
+      </b-modal>
+    <p> <br> </p>
+    <div class="tile is-ancestor is-vertical">
+      <div class="tile is-parent">
+        <article class="tile is-child is-5 is-centered">
+          <playerPanel v-on:selected='setSelected($event)'/>
+        </article>
+        <div class="tile is-parent is-vertical is-12">
+        <article class="tile notification is-child is-primary is-5 is-centered">
+          <h1 class="is-size-4"> Partie débutée le : </h1>
+          <h1 class="date is-size-2 is-centered has-text-dark">  {{ date }}</h1>
+          <h1 class="is-size-4"> En cours depuis : </h1>
+          <h1 class="time is-size-1 has-text-white">{{ time }}</h1>
+        </article>
+        <button class="button tile is-child is-primary is-medium is-5"
+          @click="Pause= true">
+          Pause
+        </button>
+        <b-modal :active.sync="Pause" scroll="keep">
+        <b-message title="Pause" class= "is-primary has-text-centered is-size-5">
+           <article class="is-centered has-text-centered"> La partie est en pause, aucune action n'est plus possible et le timer a été mis en pause. <br> Pour reprendre la partie, cliquer sur play </article>
+        </b-message>
+      </b-modal>
+        <button class="button tile is-child is-primary is-medium is-5"
+          @click="Play = true">
+          Play
+        </button>
+        <b-modal :active.sync="Play" scroll="keep">
+        <b-message title="Play" class= "is-primary has-text-centered is-size-5">
+           <article class="is-centered has-text-centered"> La partie a repris </article>
+        </b-message>
+      </b-modal>
+        </div>
+      </div>
   </div>
+</div>
 </template>
+
+<script>
+
+import playerPanel from '@/components/PlayerPanel.vue';
+
+export default {
+  name: 'Overview',
+  components: { playerPanel },
+  data() {
+    return {
+      isAnnModalActive: false,
+      date: '14/09/2020',
+      time: '12:00:01',
+      Pause: false,
+      Play: false,
+    };
+  },
+  methods: {
+    // TODO: Handle message
+    sendAnnounce() {
+      this.$store.state.socket.emit('announcement', this.announce);
+      this.isAnnModalActive = false;
+    },
+    setSelected(_SelectedKey) {
+      this.selectedKey = _SelectedKey;
+    },
+  },
+};
+</script>
+
 
 <style scoped>
 .card {
@@ -34,23 +98,3 @@
   color: white;
 }
 </style>
-
-<script>
-/* eslint-disable func-names */
-/* eslint-disable object-shorthand */
-export default {
-  name: 'Overview',
-  data() {
-    return {
-      isAnnModalActive: false,
-    };
-  },
-  methods: {
-    // TODO: Handle message
-    sendAnnounce: function () {
-      this.$store.state.socket.emit('announcement', this.announce);
-      this.isAnnModalActive = false;
-    },
-  },
-};
-</script>

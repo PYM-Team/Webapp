017fa5df6baee6ab4e6d6533696cd1f551e58210
LeonardGr <leonard.grynfogel@gmail.com>
2020-05-13 14:58:29 +0200

    #27  Optimisation - Optimisation connexion

--- src/components/HomeNavBar.vue
+++ src/components/HomeNavBar.vue
@@ -31,6 +31,7 @@
     <b-modal :active.sync="log" scroll="keep">
         <b-message title="Log in" class= "is-primary has-text-centered is-size-5">
             <div class="field">
+            <label v-if="error === true" class =" is-danger">  <b>Game ID ou prénom incorrect </b> </label>
             <label class="label">Identifiant de partie</label>
             <div class="control">
                 <input class="input" type="number" v-model="Game.id" placeholder="135644" maxlength="6">
@@ -39,12 +40,12 @@
             <div class="field">
             <b-field label="Prenom d'un joueur">
             <b-input type="password"
-                value="Tom"
+                value="Tom" v-model="Game.name"
                 password-reveal>
             </b-input>
         </b-field>
             </div>
-            <b-button tag="router-link" :to="{ path: '/Choice' }"  class="button is-primary" @click="Submit = true" >Valider</b-button>
+            <b-button  class="button is-primary" @click="Submit()" >Valider</b-button>
         </b-message>
   </b-modal>
 </div>
@@ -56,12 +57,33 @@ export default {
   data() {
     return {
       log: false,
-      Submit: false,
+      error: false,
       Game: {
         id: '',
         name: '',
       },
+      Receive: {
+        name: ['jean', 'toto', 'jacques'],
+      },
     };
+  },
+  methods: {
+    Submit() {
+      // eslint-disable-next-line no-unused-vars
+      let valid = false;
+      // sent Game id
+      // receive name
+      for (let i = 0; i < this.Receive.name.length; i += 1) {
+        if (this.Game.name === this.Receive.name[i]) {
+        // tag="router-link" :to="{ path: '/Choice' }
+          valid = true;
+          this.$router.push({ path: '/Choice' });
+        }
+      }
+      if (valid === false) {
+        this.error = true;
+      }
+    },
   },
 };
 </script>

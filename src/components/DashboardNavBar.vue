573d6f5126b9d17ff0ef53eeac1f0b5d6848fc46
LeonardGr <leonard.grynfogel@gmail.com>
2020-05-23 17:34:28 +0200

    #34 - Rajout du bouton save/quit + modal - Non effectif

--- src/components/DashboardNavBar.vue
+++ src/components/DashboardNavBar.vue
@@ -1,17 +1,64 @@
 <template>
-    <b-navbar id="dashboard-nav"> <!-- barre de navigation du dashboard  renvoyant à l'acceuil-->
-        <template slot="brand">
-            <b-navbar-item tag="router-link" :to="{ path: '/' }">
-                <img
-                    src="https://i.imgur.com/vmOM3ZT.png"
-                    alt="Lightweight UI components for Vue.js based on Bulma"
-                >
-            </b-navbar-item>
-        </template>
-        <template slot="start">
-        </template>
-    </b-navbar>
+    <b-navbar id="home-nav"> <!-- barre de navigation avec un bouton home et un autre identification ouvrant un modal -->
+      <template slot="brand">
+          <b-navbar-item tag="router-link" :to="{ path: '/' }">
+              <img
+                  src="https://i.imgur.com/vmOM3ZT.png"
+                  alt="Lightweight UI components for Vue.js based on Bulma"
+              >
+          </b-navbar-item>
+      </template>
+
+      <template slot="end">
+          <b-navbar-item tag="div">
+              <div class="buttons">
+                  <a class="button is-primary" @click="Save">
+                      Sauvegarder
+                  </a>
+              </div>
+          </b-navbar-item>
+          <b-navbar-item tag="div">
+              <div class="buttons">
+                  <a class="button is-primary" @click="quit = true">
+                      Sauvegarder et quitter
+                  </a>
+              </div>
+          </b-navbar-item>
+          <b-modal :active.sync="quit" scroll="keep"> <!-- le modal demande confirmation avant de quitter -->
+            <b-message class= "is-primary has-text-centered is-size-5">
+            <article class="title is-centered has-text-black has-text-centered"> Voulez vous vraiment quitter la partie en cours ? </article>
+            <article class="is-centered is-size-3 has-text-centered"> Notez bien votre numéro de partie : <strong>{{this.$store.state.Game.gameId}}</strong> </article> <br>
+            <b-button class="button is-primary tile is-centered is-12" @click="Quitter"> Salut ! </b-button>
+            </b-message>
+          </b-modal>
+      </template>
+  </b-navbar>
 </template>
+
+<script>
+export default {
+  name: 'DashboardNavBar',
+
+  data() {
+    return {
+      quit: false,
+
+    };
+  },
+  methods: {
+    Save() {
+      // save
+      this.$buefy.toast.open({
+        message: 'Partie sauvegardé ! ',
+        type: 'is-success',
+      });
+    },
+    Quitter() {
+      this.$router.push({ path: '/' });
+    },
+  },
+};
+</script>
 
 <style>
 #dashboard-nav {

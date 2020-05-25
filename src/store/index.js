24e6c1a34af2dc55a34e208ae20ce542c366f24e
Little-Bro <tom.ericdelmas@gmail.com>
2020-05-25 17:37:12 +0200

    ajout de la liste d'événements
    pour l'instant elle est hardcodée mais faudra la choper depuis l'API

--- src/store/index.js
+++ src/store/index.js
@@ -39,6 +39,10 @@ export default new Vuex.Store({
       description: 'Cette enquête se déroule dans les années 30, en plein coeur de la mafia italienne. Le parrain Don Giorgio a été assassiné. Qui a pu commettre une telle atrocité ? Qui va hériter de son empire et de sa fortune ? Toutes ces questions trouveront leur réponse ce soir.',
       duree: 10,
       Players: [],
+       events: [
+        { nom: 'start', description: 'lance la partie', declenche: true },
+        { nom: 'fin', description: 'termine la partie', declenche: false },
+      ],
     },
   },
   mutations: {

dad813e33da717b4dd59334af629dcc15795df83
Little-Bro <tom.ericdelmas@gmail.com>
2020-05-25 17:06:21 +0200

    tej tout ce qui était inutile

--- src/components/Annonce.vue
+++ src/components/Annonce.vue
@@ -1,23 +1,5 @@
-<template> <!-- Associe avec tile un menu avec les joueurs connectés et lorsque l'on clique sur l'un sa carte détaillée-->
-<!-- <div id="player-view" class="tile is-ancestor">
-  <div class="tile is-parent">
-    <article class="tile is-child is-12 is-centered">
-      <PlayerPanel v-on:selected='setSelected($event)'/>
-    </article>
-  </div>
-  <div class="tile is-vertical is-8">
-    <div class="tile">
-      <div class="tile is-parent is-vertical">
-        <span class="tile is-child notification is-primary">Please select a Player to show the detailled PlayerCard</span>
-        <p class="tile is-child">
-          <PlayerCard :title='selectedKey' />
-        </p>
-      </div>
-    </div>
-  </div>
-</div> -->
-
-  <div id="Evenement">
+<template>
+  <div id="Annonce">
     <b-field label="Annonce">
         <b-input maxlength="100" size="is-medium" v-model="texteAnnonce"></b-input>
     </b-field>
@@ -26,9 +8,6 @@
 </template>
 
 <script>
-// import PlayerPanel from '@/components/PlayerPanel.vue';
-// import PlayerCard from '@/components/PlayerCardComplete.vue';
-
 export default {
   name: 'Annonce',
   data() {
@@ -65,22 +44,6 @@ export default {
     },
   },
 };
-//   name: 'PlayerView',
-//   data() {
-//     return {
-//       selectedKey: {},
-//       prefered: {},
-//     };
-//   },
-//   components: { PlayerPanel, PlayerCard },
-//   methods: {
-//     setSelected(_SelectedKey) {
-//       this.selectedKey = _SelectedKey;
-//       // this.selectedKey = _SelectedKey[1];
-//     },
-//   },
-// };
-
 </script>
 
 <style>

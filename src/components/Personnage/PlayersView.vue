ef2c31b01e2b300d6f59fa00a3b078e8593ad38a
Little-Bro <tom.ericdelmas@gmail.com>
2020-05-26 18:14:45 +0200

    Préparation connextion API
    commentaire à décommenter

--- src/components/Personnage/PlayersView.vue
+++ src/components/Personnage/PlayersView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="columns is-multiline is-desktop"> <!--affiche la carte résumé de tous les joueurs -->
-    <div class="column is-3" v-for="(player, key) in this.$store.state.players" v-bind:key="key">
+    <div class="column is-3" v-for="(player, key) in this.$store.state.players" v-bind:key="key"> <!--this.$store.state.Game.PlayersDetailed-->
         <PlayerCard v-if="player.connected" :num="key" />
     </div>
   </div>

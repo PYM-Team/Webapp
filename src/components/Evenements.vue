2f5c33489036008aa4405990cb52a75538a4c3b0
LeonardGr <leonard.grynfogel@gmail.com>
2020-05-26 11:05:27 +0200

    #38#31 Ajout Evenement card

--- src/components/Evenements.vue
+++ src/components/Evenements.vue
@@ -1,14 +1,30 @@
 <template>
-  <ul id="item-list">
-    <li v-for="(event, index) of evenements" v-bind:key="index" :id="event.nom">
-    <b-tooltip v-bind:label="[event.declenche? Done : NotDone]">
-            <b-message class="is-dark">
-                {{ event.nom }} : {{ event.description }}
-            </b-message>
-        </b-tooltip>
-
-
-    </li>
+  <ul id="item-list" class = "tile is-ancestor is-vertical">
+    <br><br>
+    <li class="tile is-child is-12" v-for="(event, index) of evenements" v-bind:key="index" :id="event.nom">
+    <b-tooltip v-bind:label="[event.declenche? Done : NotDone]" position="is-right">
+      <div class="card">
+        <header class="card-header">
+          <h1 class="card-header-title title has-text-black is-centered">
+            {{event.nom}}
+          </h1>
+        </header>
+        <div class="card-content">
+          <div class="content">
+          {{ event.description}}
+            <br>
+            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+          </div>
+        </div>
+        <footer class="card-footer">
+          <a v-if='event.declenche === true' v-on:click="RelancerEvent()" class="card-footer-item">Relancer cet evenement</a>
+          <a v-if='event.declenche === false' v-on:click="lancerEvent()" class="card-footer-item">Lancer cet evenement</a>
+        </footer>
+      </div>
+      <br>
+    </b-tooltip>
+    <br>
+  </li>
 </ul>
 </template>
 
@@ -19,9 +35,17 @@ export default {
     return {
       evenements: this.$store.state.Game.events,
       Fait: false,
-      Done: 'hey',
-      NotDone: 'Not hey',
+      Done: 'Fait',
+      NotDone: 'Pas fait',
     };
+  },
+  methods: {
+    lancerEvent() {
+      console.log('eventlance');
+    },
+    RelancerEvent() {
+      console.log('eventrelance');
+    },
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import VueNativeSock from 'vue-native-websocket';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-
 import 'buefy/dist/buefy.css';
 
 

161ff46d954800675a3fa3c59c831e635dc328dc
Little-Bro <tom.ericdelmas@gmail.com>
2020-05-27 19:57:16 +0200

    communication avec API ! Wouhou !

--- src/components/Personnage/PlayerCard.vue
+++ src/components/Personnage/PlayerCard.vue
@@ -3,17 +3,16 @@
     <header class="card-header">
       <p class="card-header-title is-centered player-name">
         <img class="player-profile-pic" src="https://i.imgur.com/SfvDmCx.png"/>
-        <br /> {{this.$store.state.players[this.num].prefered}} <!--this.$store.state.Game.PlayersDetailed[this.num].name-->
+        <br /> {{this.$store.state.Game.PlayersDetailed[this.num].role}}
       </p>
     </header>
     <div class="card-content">
       <div class="content player-role">
         <p>
-          Joueur : {{this.$store.state.players[this.num].name}} <!--this.$store.state.Game.PlayersDetailed[this.num].name-->
+          Joueur : {{this.$store.state.Game.PlayersDetailed[this.num].description}}
         </p>
-        <p>
-          Etat : Vivant
-        </p>
+        <p v-if="this.$store.state.Game.PlayersDetailed[this.num].state === true">Etat : conscient</p>
+        <p v-if="this.$store.state.Game.PlayersDetailed[this.num].state === false">Etat : inconscient</p>
       </div>
     <button class="button is-primary is-medium is-pulled-right" @click="modalInv = true">Voir inventaire</button>
     <button class="button is-primary is-medium if-pulled-left" @click="modalRel = true">Voir relations</button>
@@ -39,13 +38,9 @@ export default {
       isActive: true,
       modalInv: false,
       modalRel: false,
-      // donnesInv: this.$store.state.Game.PlayersDetailed[this.num].inventory,
-      // donnesRel: this.$store.state.Game.PlayersDetailed[this.num].relations,
-      donneesInv: [
-        { objet: 'Flasque de Tequila', description: 'Un petit remontant pour se donner du courage' },
-        { objet: 'Flingue', description: 'Pour El Sampico, cette arme est une extension de sa volonté' },
-        { objet: 'Note de l’hôtel', description: 'Une note de l’hôtel où il a passé la nuit avec Carla le soir du meurtre. Elle aurait été l’alibi parfait si elle ne révélait pas en même temps sa relation secrète et incriminante avec sa dulcinée' },
-      ],
+      donneesInv: this.$store.state.Game.PlayersDetailed[this.num].inventory,
+      donneesRel: this.$store.state.Game.PlayersDetailed[this.num].relations,
+
       colonnesInv: [
         {
           field: 'name',
@@ -59,18 +54,14 @@ export default {
           field: 'clue',
           label: 'Indice',
         },
-      ], // VOIR LES NOMS QU'ILS ONT DONNE DANS L'API
-      donneesRel: [
-        { perso: 'Petro Francesco', pensee: 'Le gars a l’air solide, il à l’oeil et le corps entraînés, ça se voit' },
-        { perso: 'Tomasso Giorgio', pensee: 'Le fils à papa. Le gringo n’a jamais touché à un flingue de sa vie' },
       ],
       colonnesRel: [
         {
-          field: 'perso',
+          field: 'target',
           label: 'Personnage',
         },
         {
-          field: 'pensee',
+          field: 'thought',
           label: 'Ce que je pense du personnage',
         },
       ],
@@ -90,5 +81,4 @@ export default {
   border-radius: 5rem;
   margin-right: 1rem;
 }
-
 </style>

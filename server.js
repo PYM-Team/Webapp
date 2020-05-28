d6e96fec11da341ca114339e5611559a4505c75e
LeonardGr <leonard.grynfogel@gmail.com>
2020-05-28 18:55:30 +0200

    Change the tab logo

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-/* eslint-disable no-undef */
 const express = require('express');
-const serveStatic = require('serve-static');
 const path = require('path');
+const serveStatic = require('serve-static');
 
-app = express();
+const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 5000;
+
 app.listen(port);
+console.log(`server started on port ${port}`);
